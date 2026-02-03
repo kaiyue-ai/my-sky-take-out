6c03d80babaf6de291923e531aa59207037d34a1
kaiyue-ai <2144246846@qq.com>
2026-02-04 06:34:09 +0800

    pref:给用户端分类查询加上缓存

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/CategoryController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,7 @@ public class CategoryController {
      */
     @GetMapping("/list")
     @ApiOperation("分类查询")
+    @Cacheable(cacheNames = "category",key = "'type_'+#type")
     public Result<List<Category>> list(Integer type) {
         log.info("分类查询{}", type);
         List<Category> list = categoryService.list(type);
